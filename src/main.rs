67ee29a811ff5bda2334b19c0dbe710e63226526
foodhype <brianschmitz.main@gmail.com>
2014-12-07 17:08:12 -0800

    in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+
+struct SuggestTree<'a> {
+    root: CompletionTrie<'a>,
+    completion_table: Vec<&'a str>,
+    inverted_index: HashMap<&'a str, uint>,
+    last_index: uint
+}
+
+
+struct CompletionTrie<'a> {
+    children: HashMap<&'a str, Option<&'a CompletionTrie<'a>>>,
+}
+
+
+impl<'a> SuggestTree<'a> {
+/*    fn new() -> SuggestTree<'a> {
+        SuggestTree<'a> {
+            root: CompletionTrie<'a>,
+        }
+    }
+*/
+    fn insert(&self, word: &str, index: uint) {
+
+    }
+
+}
+
+
 fn main() {
     println!("Hello, world!")
 }
